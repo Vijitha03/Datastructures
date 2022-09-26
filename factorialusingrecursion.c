0c7f5e823885526eae35fbdfb0d1a7b6b6123916
Vijitha03 <113349410+Vijitha03@users.noreply.github.com>
2022-09-26 19:06:25 +0530

    Add files via upload

---
+++ factorialusingrecursion.c
@@ -0,0 +1,16 @@
+#include<stdio.h>  
+int main()    
+{   
+    printf("NAME: YADDALA VIJITHA  REG.NO: -192124187");
+    printf("\n");   
+ int i,fact=1,number;    
+ printf("Enter a number: ");    
+  scanf("%d",&number);    
+    for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  printf("Factorial of %d is: %d",number,fact);    
+return 0;  
+} 
+ 
+   
