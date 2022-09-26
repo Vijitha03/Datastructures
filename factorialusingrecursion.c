f0b3ef97ca92472a79e39f9430f05effa0547030
Vijitha03 <113349410+Vijitha03@users.noreply.github.com>
2022-09-26 19:07:30 +0530

    Update factorialusingrecursion.c

--- factorialusingrecursion.c
+++ factorialusingrecursion.c
@@ -12,5 +12,8 @@ int main()
   printf("Factorial of %d is: %d",number,fact);    
 return 0;  
 } 
- 
+ SAMPLE OUTPUT:
+NAME: YADDALA VIJITHA  REG.NO: -192124187
+Enter a number: 5
+Factorial of 5 is: 120
    
