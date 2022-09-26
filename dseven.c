75263c9bc7ab15bb6907467925bb3f791aa30195
Vijitha03 <113349410+Vijitha03@users.noreply.github.com>
2022-09-26 13:23:31 +0530

    Add files via upload

---
+++ dseven.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("NAME: YADDALA VIJITHA  REG.NO: -192124187");
+ 	printf("\n");
+	int a;
+	scanf("%d",&a);
+	if(a%2==0)
+	  printf("it is a even number");
+	else
+	printf("odd number");
+	return 0;
+}
