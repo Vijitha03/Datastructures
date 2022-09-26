36c5f7af7d9d299d347b3772074a4fc1c0b30811
Vijitha03 <113349410+Vijitha03@users.noreply.github.com>
2022-09-26 18:18:27 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ int main()
+ {
+ 	printf("NAME: YADDALA VIJITHA  REG.NO: -192124187");
+ 	printf("\n");
+ 	
+ 	int x,y,a;
+ 	scanf("%d %d",&x,&y);
+ 	a=x+y;
+ 	printf("the sum of x and y is:" "%d",a);
+ 	return 0;
+ }
+ SAMPLE OUTPUT:
+ 	NAME: YADDALA VIJITHA  REG.NO: -192124187
+4
+9
+the sum of x and y is:13SS
