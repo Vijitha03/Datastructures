43ce1d5a12e369c2ee431c80194d8e9d55ff6fd7
Vijitha03 <113349410+Vijitha03@users.noreply.github.com>
2022-09-27 22:34:33 +0530

    Add files via upload

---
+++ linearsearch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int a[10],i,x,n;
+	printf("no of elements:");
+	scanf("%d",&n);
+	printf("enter the array elements:");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	printf("enter the element ");
+	scanf("%d",&x);
+	for(i=0;i<n;i++)
+    if(a[i]==x)
+	 break;
+	 if(i<n)	
+	 printf("element found at index %d",i);
+	 else
+	 printf("element not found");
+	 return 0;
+	 
+}
