e1e1f9cd9c566059017320693884f3999e58a26e
Vijitha03 <113349410+Vijitha03@users.noreply.github.com>
2022-09-28 21:47:39 +0530

    Update HASHING.c

--- HASHING.c
+++ HASHING.c
@@ -82,5 +82,49 @@ main()
         }
     }
 }
+ 
+SAMPLE OUTPUT:
+NAME: YADDALA VIJITHA  REG.NO: -192124187
+  Press 1. Insert  2. Display     3. Search       4.Exit
+1
 
+enter a value to insert into hash table
+6
+
+Press 1. Insert  2. Display     3. Search       4.Exit
+1
+
+enter a value to insert into hash table
+67
+
+Press 1. Insert  2. Display     3. Search       4.Exit
+1
+
+enter a value to insert into hash table
+78
+
+Press 1. Insert  2. Display     3. Search       4.Exit
+188
+
+Press 1. Insert  2. Display     3. Search       4.Exit
+2
+
+elements in the hash table are
+
+at index 0       value =  0
+at index 1       value =  0
+at index 2       value =  0
+at index 3       value =  0
+at index 4       value =  0
+at index 5       value =  0
+at index 6       value =  6
+at index 7       value =  67
+at index 8       value =  78
+at index 9       value =  0
+Press 1. Insert  2. Display     3. Search       4.Exit
+3
+
+enter search element
+6
+value is found at index 6
 
