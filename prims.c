4dd76750ca29118bb5ff2fbc2b34ef1e94dedaaf
Vijitha03 <113349410+Vijitha03@users.noreply.github.com>
2022-09-29 14:47:09 +0530

    Update prims.c

--- prims.c
+++ prims.c
@@ -36,7 +36,7 @@ int prims()
 int cost[MAX][MAX];
 int u,v,min_distance,distance[MAX],from[MAX];
 int visited[MAX],no_of_edges,i,min_cost,j;
-//create cost[][] matrix,spanning[][]
+
 for(i=0;i<n;i++)
 for(j=0;j<n;j++)
 {
@@ -46,7 +46,7 @@ else
 cost[i][j]=G[i][j];
 spanning[i][j]=0;
 }
-//initialise visited[],distance[] and from[]
+
 distance[0]=0;
 visited[0]=1;
 for(i=1;i<n;i++)
@@ -59,7 +59,7 @@ min_cost=0; //cost of spanning tree
 no_of_edges=n-1; //no. of edges to be added
 while(no_of_edges>0)
 {
-//find the vertex at minimum distance from the tree
+
 min_distance=infinity;
 for(i=1;i<n;i++)
 if(visited[i]==0&&distance[i]<min_distance)
@@ -68,12 +68,12 @@ v=i;
 min_distance=distance[i];
 }
 u=from[v];
-//insert the edge in spanning tree
+
 spanning[u][v]=distance[v];
 spanning[v][u]=distance[v];
 no_of_edges--;
 visited[v]=1;
-//updated the distance[] array
+
 for(i=1;i<n;i++)
 if(visited[i]==0&&cost[i][v]<distance[i])
 {
